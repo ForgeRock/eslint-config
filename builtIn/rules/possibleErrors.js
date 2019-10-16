2e0be4dc1280e8fe3a4479902cab00aa8f52b226
Phil Ostler <phil.ostler@forgerock.com>
2019-10-16 12:04:03 +0100

    Turn `no-console` On

--- builtIn/rules/possibleErrors.js
+++ builtIn/rules/possibleErrors.js
@@ -1,6 +1,6 @@
 module.exports = {
     rules: {
-        "no-console": "off", // Alternative required before enabling this. See OPENAM-10938.
+        "no-console": "error",
         "no-template-curly-in-string": "error",
         "no-unsafe-negation": "error"
     }
