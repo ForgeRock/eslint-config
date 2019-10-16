00073af05a5726db539b4f09c249a46a3cd75d3b
Phil Ostler <phil.ostler@forgerock.com>
2019-10-16 12:04:13 +0100

    Allow Empty Catch Blocks

--- builtIn/rules/possibleErrors.js
+++ builtIn/rules/possibleErrors.js
@@ -1,6 +1,9 @@
 module.exports = {
     rules: {
         "no-console": "error",
+        "no-empty": ["error", {
+            allowEmptyCatch: true
+        }],
         "no-template-curly-in-string": "error",
         "no-unsafe-negation": "error"
     }
